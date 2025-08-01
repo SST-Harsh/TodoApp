30a6f5f2d94d993bbb9dee36e6920801d874d674
SST-Harsh <harshpal@softsourcetech.com>
2025-08-01 16:08:21 +0530

    done

--- client/src/Pages/Form.jsx
+++ client/src/Pages/Form.jsx
@@ -1,7 +1,16 @@
+import {useForm} from 'react-hook-form';
 export default function Form(){
-    
+     const { register, handleSubmit, formState } = useForm();
+        const onSubmit=(data)=>{
+            console.log(data)
+        }
     return(
         <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type='text' {...register('name', {required: true})} placeholder='Name' className='border p-2 rounded mb-2'/>
+            <input type='email' {...register('email', {required: true})} placeholder='Email' className='border p-2 rounded mb-2'/>
+            <button type='submit' className='bg-blue-500 text-white p-2 rounded'>Submit</button>
+        </form>
         </>
     )
 }
